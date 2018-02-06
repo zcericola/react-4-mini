b9fb01b937b33d7fa7642aa7b0d433a2982346d6
Zac Cericola <zcericola@gmail.com>
2018-02-06 12:12:52 -0600

    finished react-mini-4

---
+++ src/Route1.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Route1 = () => {
+     return <h1>Route 1 here!</h1>;
+
+}
+
+export default Route1;
---
+++ src/Route2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Route2 = () => {
+ return <h1>Route 2 here!</h1>;
+
+};
+
+export default Route2;
